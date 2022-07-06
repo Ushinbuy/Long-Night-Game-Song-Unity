70a8d904e41662bd32a9da0fed84f069b1ae8543
Ushinbuy <n.s.shadrin@gmail.com>
2022-07-06 10:09:13 +0300

    Add Global Scenario fro Level 1

---
+++ Assets/Scripts/CommonScenariosDelegates.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class CommonScenariosDelegates : MonoBehaviour
+{
+    delegate void FinalBattery();
+    delegate void BossStart();
+    delegate void EndLevel();
+
+    FinalBattery finalBattery;
+
+    private static ScenariosState nextScenariosState;
+    private enum ScenariosState
+    {
+        NONE,
+        START_LEVEL,
+        BOSS_START,
+        FINAL_BATTERY,
+        END_LEVEL_1
+    }
+
+    private void Start()
+    {
+        nextScenariosState = ScenariosState.START_LEVEL;
+        finalBattery += NextScenarioStep;
+    }
+
+    private void NextScenarioStep()
+    {
+        nextScenariosState++;
+    }
+}
